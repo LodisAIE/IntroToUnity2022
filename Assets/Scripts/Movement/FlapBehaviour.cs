0cfb91b112e8a94710d31d26a5f7363ac5abfbdf
Lodis Perkins <lodisperkins44@gmail.com>
2021-03-31 17:08:03 -0500

    Added sound effects and floating pipes

--- Assets/Scripts/Movement/FlapBehaviour.cs
+++ Assets/Scripts/Movement/FlapBehaviour.cs
@@ -6,6 +6,7 @@ public class FlapBehaviour : MonoBehaviour
 {
     private Rigidbody _rigidbody;
     public float FlapForce = 5.0f;
+    public AudioSource JumpAudioSource;
 
     // Start is called before the first frame update
     void Start()
@@ -13,10 +14,19 @@ public class FlapBehaviour : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody>();   
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+            Destroy(gameObject);
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Flap"))
+        {
             _rigidbody.AddForce(new Vector3(0, FlapForce, 0), ForceMode.Impulse);
+            JumpAudioSource.Play();
+        }  
     }
 }
